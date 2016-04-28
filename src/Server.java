967148873f6b7a5714e3644617778b19a555086f
Oscar <oscardasilva@mbp-de-oscar.einet.ad.eivd.ch>
2016-04-28 20:35:00 +0200

    Ajout IServer interface et modification Server

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,18 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class Server {
+public class Server extends UnicastRemoteObject implements IServer{
+
+	protected Server() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long serialVersionUID = 1L;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
