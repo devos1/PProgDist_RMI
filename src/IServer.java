82500ba7a2b19a72284ee7e827ef219c9d66fdff
Brait Loic <loic@brait.ch>
2016-04-28 21:00:19 +0200

    Début mise en place RMI

--- src/IServer.java
+++ src/IServer.java
@@ -1,5 +1,9 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface IServer extends Remote {
+	void addUtilisateur(IUtilisateur utilisateur) throws RemoteException;
+
+	//void send(Item chatItem) throws RemoteException;
 	
 }
--- src/Server.java
+++ src/Server.java
@@ -1,19 +1,40 @@
+import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 public class Server extends UnicastRemoteObject implements IServer{
 
+	// MAIN
+	public static void main(String[] args) throws RemoteException, AlreadyBoundException {
+		IServer server = new Server();
+		Registry registry = LocateRegistry.createRegistry(1099);
+		registry.bind("Server", server);
+
+	}
+	
+	private static final long serialVersionUID = 1L;
+	private ArrayList<IUtilisateur> utilisateurs;
+	
 	protected Server() throws RemoteException {
 		super();
-		// TODO Auto-generated constructor stub
+		
+		utilisateurs = new ArrayList<IUtilisateur>();
+	}
+	
+	public void send(String texte) {
+		for (IUtilisateur user : utilisateurs) {
+			//user.display(texte);
+		}
 	}
 
-	private static final long serialVersionUID = 1L;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Override
+	public void addUtilisateur(IUtilisateur utilisateur) throws RemoteException {
+		utilisateurs.add(utilisateur);
 		
-
 	}
 
 }
