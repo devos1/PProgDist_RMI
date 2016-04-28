caa55898987dd52edbb37f88c909fc93344fa424
Oscar <oscardasilva@mbp-de-oscar.einet.ad.eivd.ch>
2016-04-28 20:57:11 +0200

    Création AppFrame et motif Utilisateur

---
+++ src/AppFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class AppFrame extends JFrame {
+	
+	public AppFrame(){
+		super("Location de véhicules dans toute la Suisse");
+		
+		
+		
+		setSize(600, 400);
+		setVisible(true);
+	}
+	
+}
--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,10 +1,21 @@
-public class Utilisateur {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Utilisateur extends UnicastRemoteObject implements IUtilisateur{
 
 	private String nom;
 
 	private String prenom;
 
 	private Vehicule vehicule;
+	
+	public static void main(String[] args) {
+		new AppFrame();
+	}
+	
+	protected Utilisateur() throws RemoteException {
+		super();
+	}
 
 	public int login() {
 		return 0;
