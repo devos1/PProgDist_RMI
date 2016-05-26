7bd3123317b1036ed90102bec6e2c8aaf9e0482d
Brait Loic <loic@brait.ch>
2016-05-26 19:14:27 +0200

    RMI
    correction de l'erreur de déclaration d'utilisateur sur le serveur

--- src/IUtilisateur.java
+++ src/IUtilisateur.java
@@ -1,7 +1,8 @@
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public interface IUtilisateur {
+public interface IUtilisateur extends Remote{
 	void majPlaces(Place place, int nbPlaceLoue) throws RemoteException;
 	void majStations(ArrayList<Station> stations) throws RemoteException;;
 
